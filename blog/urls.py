6a704bf6ff6e70d9fd8e2fd4c4a1dbe875f33934
niamhbrowne <niamh_browne@icloud.com>
2022-01-24 21:25:48 +0000

    added comments to files

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('packages/', views.packages, name='packages'),
+    path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,30 @@
 from django.views import generic
 from .models import Post
+from django.shortcuts import render
 
 # Create your views here.
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
 
+
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+def about(request):
+    context = {}
+    return render(request, 'about.html', context)
+
+
+def contact(request):
+    context = {}
+    return render(request, 'contact.html', context)
+
+
+def packages(request):
+    context = {}
+    return render(request, 'packages.html', context)
