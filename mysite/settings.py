e34c7b06902453e0734db5a89554a2ea81f3de31
niamhbrowne <niamh_browne@icloud.com>
2022-02-01 12:13:36 +0000

    removed secret key and added debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g-@#*#6*o4eppl2gfpob+@4qutkj%c+cgl#1jhq&^bdmc39-6@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cooking-guide-ms4.herokuapp.com', 'localhost']
 
